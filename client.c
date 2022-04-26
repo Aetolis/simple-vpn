71c153ddd640f7e2e794a20d47bd579ff72573f1
Riley Collins <collin_r2@denison.edu>
2022-04-25 20:14:25 -0400

    client file

---
+++ client.c
@@ -0,0 +1,20 @@
+// client.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <poll.h>
+
+#define PORT "9034"
+
+
+int main(int argc, char **argv){
+
+    return 0;
+}
